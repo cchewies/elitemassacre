712a92a4b01e32ba68ff2bde976985e14e2cb3ff
cchewies <retrochewy823@gmail.com>
2025-07-13 22:44:20 -0400

    very primitive kill tracker

--- src/MassacreHelper.py
+++ src/MassacreHelper.py
@@ -4,48 +4,44 @@ import MissionTracker
 import json
 
 def loadAllJournals():
-  accept = []
-  complete = []
   journals = ReadJournal.readAllJournals()
   for event in journals:
     if event['event'] == "MissionAccepted":
-      accept.append(event)
+      MissionTracker.acceptMission(event)
     if event['event'] == "MissionCompleted":
-      complete.append(event)
-
-  for event in accept:
-    MissionTracker.acceptMission(event)
-  for event in complete:
-    MissionTracker.completeMission(event)
+      MissionTracker.completeMission(event)
+    if event['event'] == "MissionAbandoned":
+      MissionTracker.completeMission(event)
+    if event['event'] == "MissionFailed":
+      MissionTracker.completeMission(event)
+    if event['event'] == "Bounty":
+      MissionTracker.processKill(event)
 
 def runMassacreHelper():
   loadAllJournals()
   data = []
   while 1:
-    accept = []
-    complete = []
     data = ReadJournal.fetchJournalUpdate(data)
 
     for event in data:
       if event['event'] == "MissionAccepted":
-        accept.append(event)
+        MissionTracker.acceptMission(event)
       if event['event'] == "MissionCompleted":
-        complete.append(event)
+        MissionTracker.completeMission(event)
       if event['event'] == "MissionAbandoned":
-        complete.append(event)
+        MissionTracker.completeMission(event)
       if event['event'] == "MissionFailed":
-        complete.append(event)
+        MissionTracker.completeMission(event)
+      if event['event'] == "Bounty":
+        MissionTracker.processKill(event)
 
-    for event in accept:
-      MissionTracker.acceptMission(event)
-    for event in complete:
-      MissionTracker.completeMission(event)
-
+    print("================== NEW EVENT ==================")
     factions = MissionTracker.factionView()
     for faction in factions:
       s = f"{faction}: "
       for mission in factions[faction]:
-        s += f"{mission['Targets']} "
+        if mission['Kills'] < mission['Targets']:
+          s += f"{mission['Kills']}/{mission['Targets']} "
       print(s)
 
 def main():
--- src/MissionTracker.py
+++ src/MissionTracker.py
@@ -7,10 +7,18 @@ def acceptMission(event):
   if event['MissionID'] in tracker: return      # Already accepted
   tracker[event['MissionID']] = {}
   mission = tracker[event['MissionID']]
+  mission['Timestamp'] = event['timestamp']
   mission['Faction'] = event['Faction']
   mission['Targets'] = event['KillCount']
+  mission['Kills'] = 0
   mission['Reward'] = event['Reward']
   mission['Wing'] = event['Wing']
+
+def processKill(event):
+  for missionID in tracker.keys():
+    mission = tracker[missionID]
+    if event['timestamp'] > mission['Timestamp']:
+      mission['Kills'] += 1
 
 def completeMission(event):
   if not event['MissionID'] in tracker: return  # Already completed
